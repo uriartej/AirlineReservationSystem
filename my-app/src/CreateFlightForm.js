f8d6641cbfcfd705ebc1850ab72cc320a7a24f9b
uriartej <uriarte.juan.j@gmail.com>
2023-12-02 10:23:34 -0800

    added implementation to create flight

--- my-app/src/CreateFlightForm.js
+++ my-app/src/CreateFlightForm.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import CheckoutForm from './CheckoutForm';
-import './Cart.css';
 
 function CreateFlightForm() {
   const [flightDetails, setFlightDetails] = useState({
@@ -17,59 +15,69 @@ function CreateFlightForm() {
   const [seatTypes, setSeatTypes] = useState([]);
   const [streamingServices, setStreamingServices] = useState([]);
   const [foodItems, setFoodItems] = useState([]);
-  const [flightAvailability, setDestination] = useState([]);
+  const [flightAvailability, setFlightAvailability] = useState([]);
   const [submitStatus, setSubmitStatus] = useState('');
+  const [userInfo, setUserInfo] = useState({
+    fullName: '',
+    email: ''
+  });
 
   useEffect(() => {
-    // Fetch data for seat types, streaming services, and food items
     const fetchData = async () => {
       try {
-        const seatTypesRes = await axios.get('/list_seat_types');
-        const streamingServicesRes = await axios.get('/list_streaming_services');
-        const foodItemsRes = await axios.get('/list_food_items');
-        const flightAvailabilityRes = await axios.get('/available_flights');
-        
-        setSeatTypes(seatTypesRes.data);
-        setStreamingServices(streamingServicesRes.data);
-        setFoodItems(foodItemsRes.data);
-        setDestination(flightAvailabilityRes.data);
+        const responses = await Promise.all([
+          axios.get('/list_seat_types'),
+          axios.get('/list_streaming_services'),
+          axios.get('/list_food_items'),
+          axios.get('/available_flights'),
+        ]);
+        setSeatTypes(responses[0].data);
+        setStreamingServices(responses[1].data);
+        setFoodItems(responses[2].data);
+        setFlightAvailability(responses[3].data);
       } catch (error) {
         console.error("Failed to fetch data: ", error);
       }
     };
-
     fetchData();
   }, []);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post('/create_flight', flightDetails);
-      setSubmitStatus('Flight created successfully!');
-      console.log(response.data.message); // Or handle the success response as needed
-      // Clear form fields
-      setFlightDetails({
-        flightNumber: '',
-        source: '',
-        destination: '',
-        capacity: '',
-        price: '',
-        seatType: '',
-        streamingService: '',
-        foodItem: '',
-      });
-    } catch (error) {
-      setSubmitStatus('Failed to create flight.');
-      console.error("Failed to create flight: ", error);
-    }
+
+  const handleFlightChange = (event) => {
+    const { name, value } = event.target;
+    setFlightDetails(prevDetails => ({
+      ...prevDetails,
+      [name]: value,
+    }));
   };
 
-  const handleChange = (event) => {
-    setFlightDetails({
-      ...flightDetails,
-      [event.target.name]: event.target.value,
+  const handleUserInfoChange = (event) => {
+    const { name, value } = event.target;
+    setUserInfo(prevInfo => ({
+      ...prevInfo,
+      [name]: value,
+    }));
+  };
+
+  const handleUserInfoSubmit = async (event) => {
+    event.preventDefault();
+    // Here you would typically send the userInfo to the server
+    // For demonstration purposes, we'll just set the submit status
+    setSubmitStatus(`Thank you, ${userInfo.fullName}. We will email you shortly if a flight is available.`);
+
+    // Clear the form fields
+    setUserInfo({
+      fullName: '',
+      email: ''
     });
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleUserInfoSubmit(event); // Call the user info submit function
+  };
+
+
 
   return (
     <div>
@@ -82,76 +90,99 @@ function CreateFlightForm() {
           <label>
             Flight Availability:
             <select
-              name="destination"
-              value={flightDetails.destination}
-              onChange={handleChange}
+              name="flightId"
+              value={flightDetails.flightId}
+              onChange={handleFlightChange}
               required
             >
             <option value="">Select Flight</option>
-            {flightAvailability.map((type) => (
-              <option key = {type} value={type}>{type}</option>
-            ))}
+            {flightAvailability.map((flight) => (
+          <option key={flight[0]} value={flight[0]}>
+            {flight[1]} - From {flight[2]} to {flight[5]} 
+          </option>
+        ))}
             </select>
           </label>
         </div>
 
 
         {/* Select field for Seat Type */}
-        <div>
-          <label>
-            Seat Type:
-            <select
-              name="seatType"
-              value={flightDetails.seatType}
-              onChange={handleChange}
-              required
-            >
-              <option value="">Select Seat Type</option>
-              {seatTypes.map((type) => (
-                <option key={type} value={type}>{type}</option>
-              ))}
-            </select>
-          </label>
-        </div>
+<div>
+  <label>
+    Seat Type:
+    <select
+      name="seatType"
+      value={flightDetails.seatType}
+      onChange={handleFlightChange} // Updated here
+      required
+    >
+      <option value="">Select Seat Type</option>
+      {seatTypes.map((type) => (
+        <option key={type} value={type}>{type}</option>
+      ))}
+    </select>
+  </label>
+</div>
 
-        {/* Select field for Streaming Service */}
-        <div>
-          <label>
-            Streaming Service:
-            <select
-              name="streamingService"
-              value={flightDetails.streamingService}
-              onChange={handleChange}
-              required
-            >
-              <option value="">Select Streaming Service</option>
-              {streamingServices.map((service) => (
-                <option key={service} value={service}>{service}</option>
-              ))}
-            </select>
-          </label>
-        </div>
+{/* Select field for Streaming Service */}
+<div>
+  <label>
+    Streaming Service:
+    <select
+      name="streamingService"
+      value={flightDetails.streamingService}
+      onChange={handleFlightChange} // Updated here
+      required
+    >
+      <option value="">Select Streaming Service</option>
+      {streamingServices.map((service) => (
+        <option key={service} value={service}>{service}</option>
+      ))}
+    </select>
+  </label>
+</div>
 
-        {/* Select field for Food Item */}
-        <div>
-          <label>
-            Food Item:
-            <select
-              name="foodItem"
-              value={flightDetails.foodItem}
-              onChange={handleChange}
-              required
-            >
-              <option value="">Select Food Item</option>
-              {foodItems.map((item) => (
-                <option key={item} value={item}>{item}</option>
-              ))}
-            </select>
-          </label>
-        </div>
+{/* Select field for Food Item */}
+<div>
+  <label>
+    Food Item:
+    <select
+      name="foodItem"
+      value={flightDetails.foodItem}
+      onChange={handleFlightChange} // Updated here
+      required
+    >
+      <option value="">Select Food Item</option>
+      {foodItems.map((item) => (
+        <option key={item} value={item}>{item}</option>
+      ))}
+    </select>
+  </label>
+</div>
 
-        <button type="submit">Create Flight</button>
+        <label>
+          Full Name:
+          <input
+            type="text"
+            name="fullName"
+            value={userInfo.fullName}
+            onChange={handleUserInfoChange}
+            required
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={userInfo.email}
+            onChange={handleUserInfoChange}
+            required
+          />
+        </label>
+        <button type="submit">Submit</button>
       </form>
+      {submitStatus && <p>{submitStatus}</p>}
     </div>
   );
 }
